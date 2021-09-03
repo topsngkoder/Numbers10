157471176781a8141ab6ab2b884ac4edf1836869
topsngkoder <mir24zvuk@yandex.ru>
2021-09-03 16:54:01 +0300

    NUM10 #MenuWindow Prefab
    Черновая верстка окна меню

---
+++ NUM10/Assets/Scripts/MenuWindow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuWindow : MonoBehaviour
+{
+    [SerializeField]
+    private Toggle MusicToggle;
+
+    [SerializeField]
+    private Toggle SFXToggle;
+
+    [SerializeField]
+    private Button HowToPlayButton;
+
+    [SerializeField]
+    private Button BuyPremiumButton;
+
+    [SerializeField]
+    private Button ConnectButton;
+}
